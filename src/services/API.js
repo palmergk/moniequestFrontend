793f6a7a7b762670fd90244c205c4ef801b248c0
palmergk <palmergid@gmail.com>
2025-03-04 00:44:21 +0100

    updates

--- src/services/API.js
+++ src/services/API.js
@@ -8,6 +8,7 @@ export let imageurl;
 
 if (import.meta.env.VITE_ENV === 'production') {
     URL = import.meta.env.VITE_LIVE_API_URL
+    console.log(URL)
     imageurl = import.meta.env.VITE_LIVE_API_URL
 } else {
     URL = import.meta.env.VITE_API_URL
