f6a1dc6dc84c46e7a9a316d322a2808d975c716e
palmergk <palmergid@gmail.com>
2025-03-11 19:11:10 +0100

    updates

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,7 @@ import './index.css'
 import { Toaster } from 'sonner'
 import 'aos/dist/aos.css'
 import AOS from 'aos'
-import { GoogleOAuthProvider } from '@react-oauth/google';
-const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID
-if (!clientId) {
-  console.error('Google Client ID is missing. Please check your .env file.');
-}
+
 
 function Main() {
   useEffect(() => {
@@ -22,9 +18,7 @@ function Main() {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={clientId}>
     <Main />
     <Toaster />
-    </GoogleOAuthProvider>
   </React.StrictMode>,
 )
