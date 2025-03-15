fece72227ac02c9eef22bb5dd005d9c1b2f018bc
palmergk <palmergid@gmail.com>
2025-03-15 01:48:53 +0100

    updates

--- src/pages/authuser/SellCrypto.jsx
+++ src/pages/authuser/SellCrypto.jsx
@@ -23,7 +23,6 @@ const SellCrypto = () => {
     const [isPageLoading, setIsPageLoading] = useState(!navigator.onLine)
     const [loading, setLoading] = useState(false)
     const [utils] = useAtom(UTILS)
-    console.log(utils)
     const [cryptos] = useAtom(CRYPTOS)
     const [forms, setForms] = useState({
         amount: '',
@@ -238,7 +237,7 @@ const SellCrypto = () => {
                                                 </option>
                                             ))}
                                     </select>
-                                    <div className="w- text-red-600 text-xs">Please Note: you can only sell a minimum of ${forms.minimum} and maximum of ${forms.limit}. verify your account to increase limit</div>
+                                    <div className="w- text-red-600 text-xs">Please Note: you can only sell a minimum of ${forms.minimum} and maximum of ${forms.limit.toLocaleString()}. verify your account to increase limit</div>
                                 </div>
                                 <div className="flex w-full items-start gap-2 flex-col  ">
                                     <div className="font-bold text-lg">Amount:</div>
